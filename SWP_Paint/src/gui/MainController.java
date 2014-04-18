a0d91877215868f5630485d31ee25266e24cdf48
if12b054 <victor.stabrawa@hotmail.com>
2014-04-18 12:04:11 +0200

    GUI verändert + ein paar Beispiele für shape_draw

--- SWP_Paint/src/gui/MainController.java
+++ SWP_Paint/src/gui/MainController.java
@@ -2,60 +2,78 @@ package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import businesslayer.Businesslayer;
 import bussinessobjects.GuiData;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 
 public class MainController implements Initializable{
+	
 	/*Comboboxen*/
 	@FXML private ComboBox<String> cbColor;
 	@FXML private ComboBox<String> cbShape;
-	@FXML private ComboBox<String> cbPen;
-	/*Comboboxeninhalte*/
-	private ObservableList<String> cbColorData = FXCollections.observableArrayList();
-	private ObservableList<String> cbShapeData = FXCollections.observableArrayList();
-	private ObservableList<String> cbPenData = FXCollections.observableArrayList();
+	@FXML private ComboBox<Integer> cbWeight;
+	
 	/*Textfelder*/
 	@FXML TextField tfWidth;
 	@FXML TextField tfLength;
+	@FXML TextField tfRadius;
+	
+	@FXML Pane pCanvas;
+	
 	/*Buttons*/
-	@FXML private Button bClear;
-	@FXML private Button bGroup;
-	@FXML private Button bStart;
+	@FXML private Button bClear, bGroup, bShape, bEdit, bUndo, bRedo;
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		initComboboxColor();
-		initComboboxShape();
-		initComboboxPen();
+		/* Initialize Comboboxes */
+		cbShape.getItems().addAll("Kreis", "Ellipse", "Dreieck", "Quadrat", "Rechteck");
+		cbColor.getItems().addAll("Rot", "Grün");
+		cbWeight.getItems().addAll(1,2,4,8,16,32);
+		
+		Canvas canvas = new Canvas(474,482);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+        drawShapes(gc);
+		
+		pCanvas.getChildren().add(canvas);
 	}
 	
-	public void initComboboxColor(){
-		cbColorData.add("Rot");
-		cbColorData.add("Grün");
-		cbColorData.add("Gelb");
-		cbColorData.add("Schwarz");
-		cbColorData.add("Blau");
-		cbColorData.add("Weiß");
-	}
+	private void drawShapes(GraphicsContext gc) {
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
+        gc.strokeLine(40, 10, 10, 40);
+        gc.fillOval(10, 60, 30, 30);
+        gc.strokeOval(60, 60, 30, 30);
+        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
+        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
+        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
+        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
+        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
+        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
+        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
+        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
+        gc.fillPolygon(new double[]{10, 40, 10, 40},
+                       new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolygon(new double[]{60, 90, 60, 90},
+                         new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolyline(new double[]{110, 140, 110, 140},
+                          new double[]{210, 210, 240, 240}, 4);
+    }
 	
-	public void initComboboxShape(){
-		cbShapeData.add("Rechteck");
-		cbShapeData.add("Kreis");
-		cbShapeData.add("Ellipse");
-		cbShapeData.add("Dreieck");
-		cbShapeData.add("Quadrat");
-	}
-	
-	public void initComboboxPen(){
-		cbPenData.add("Dünn");
-		cbPenData.add("Dick");
+	public void initComboboxWeight(){
+		cbWeight.getItems().addAll(1,2,4,8,16,32);
 	}
 	
 	public void start(){
